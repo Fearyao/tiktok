42173c177dcb625182cf9c84cde618b50cf12b3f
fearyao <965113581@qq.com>
2018-12-02 18:05:04 +0800

    swiper

--- src/components/multSearch/multSearch.jsx
+++ src/components/multSearch/multSearch.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import cs from 'classnames'
+import style from './multsearch.scss'
 class MultSearch extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            active: 0
+        }
     }
-    render(){
-        return(
-            <div className="multSearch">
-                multSearch
+    setItem = (num) => {
+        this.setState({
+            active: num
+        }, this.getList)
+    }
+    getList = () => {
+        console.log(this.state.active)
+    }
+    render() {
+        return (
+            <div className={style.multsearch}>
+                <div className={cs(style.item)}>
+                    <span className={cs('fa', 'fa-search')}></span>
+                </div>
+                <div className={style.item}>
+                    <span className={this.state.active === 0 ? style.active : ''} onClick={this.setItem.bind(this, 0)}>推荐</span>
+                    <span className={this.state.active === 1 ? style.active : ''} onClick={this.setItem.bind(this, 1)}>杭州</span>
+                </div>
+                <div className={style.item}>
+                    <span className='fa fa-qrcode'></span>
+                </div>
             </div>
         )
     }
--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,22 +1,82 @@
 import React, { Component } from 'react'
 import MultSearch from '../../components/multSearch/multSearch'
 import NavBar from '../../components/navBar/navBar'
-import cs from 'classnames'
+import TikComponent from '../../components/tik/tik'
 import style from './homePage.scss'
-
+import cs from 'classnames'
+import Swiper from 'swiper'
+import swiperStyle from 'swiper/dist/css/swiper.min.css'
 class HomePage extends Component {
     constructor(props) {
         super(props)
         this.state = {}
     }
+    componentDidMount() {
+        new Swiper('.swiper-container', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            direction: "vertical"
+        })
+    }
     render() {
         return (
-            <div className={cs(style.homePage)}>
-                <div className={cs(style.top)}>
+            <div className={style.homePage}>
+                <div className={style.top}>
                     <MultSearch></MultSearch>
                 </div>
+                <div className={style.content}>
 
-                <div className={cs(style.bottom)}>
+                    <div className={cs("swiper-container", swiperStyle['swiper-container'], style.container)}>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+                        <div className={cs("swiper-wrapper", swiperStyle['swiper-wrapper'], style.wrapper)}>
+                            <div className={cs("swiper-slide", swiperStyle['swiper-slide'], style.slide)}>
+                                <TikComponent></TikComponent>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div className={style.bottom}>
                     <NavBar></NavBar>
                 </div>
             </div>
